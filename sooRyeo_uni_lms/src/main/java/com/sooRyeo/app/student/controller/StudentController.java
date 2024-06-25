28406635c6e22a3935ad335886439c40619977dd
Son <162323298+sssyyyggg@users.noreply.github.com>
2024-06-25 16:20:08 +0900

    [손영관] 학생메인 커밋

--- sooRyeo_uni_lms/src/main/java/com/sooRyeo/app/student/controller/StudentController.java
+++ sooRyeo_uni_lms/src/main/java/com/sooRyeo/app/student/controller/StudentController.java
@@ -1,5 +1,28 @@
 package com.sooRyeo.app.student.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class StudentController {
 
+	@RequestMapping(value = "/student/student_Main.lms", method = RequestMethod.GET)
+	public String student() {
+
+		return "student_Main.student";
+		// /WEB-INF/views/student/{1}.jsp
+	}
+	
+	@RequestMapping(value = "/student/lectureList.lms", method = RequestMethod.GET)
+	public String lectureList() {
+		
+		return "lectureList.student";
+		// /WEB-INF/views/student/{1}.jsp
+	}
+	
+	
+	
+	
+	
 }
