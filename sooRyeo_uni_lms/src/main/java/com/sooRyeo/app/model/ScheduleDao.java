d2b355ab2669e3d4467fbddcf4789d0eb37f5468
gothsteed <124808591+gothsteed@users.noreply.github.com>
2024-07-28 00:53:43 +0900

    시험 서약서 동의창, 시험 응시 리다이렉트

--- sooRyeo_uni_lms/src/main/java/com/sooRyeo/app/model/ScheduleDao.java
+++ sooRyeo_uni_lms/src/main/java/com/sooRyeo/app/model/ScheduleDao.java
@@ -5,11 +5,7 @@ import java.util.Map;
 
 import com.sooRyeo.app.domain.Consult;
 import com.sooRyeo.app.domain.Exam;
-import com.sooRyeo.app.domain.Schedule;
 import com.sooRyeo.app.dto.ConsultApprovalDto;
-import com.sooRyeo.app.dto.ExamDTO;
-
-import org.springframework.http.ResponseEntity;
 
 public interface ScheduleDao {
 	
@@ -61,7 +57,7 @@ public interface ScheduleDao {
 
 	int getStudentConfirmedConsultCount(int studentId);
 
-	Exam getExam(String schedule_seq);
+	Exam getExam(int schedule_seq);
     List<Exam> getExamList(int currentPage, int sizePerPage, int courseSeq);
 
 	int getExamCount(int courseSeq);
